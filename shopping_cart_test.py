8843eec6cf838c4f451b57dd3557b961c6db36c0
gcb62 <46456419+gcb62@users.noreply.github.com>
2019-05-08 14:54:51 -0400

    Intermediate challenge

--- shopping_cart_test.py
+++ shopping_cart_test.py
@@ -1,7 +1,11 @@
 # shopping_cart_test.py
 
+import pytest
+
 from shopping_cart import to_usd
 from shopping_cart import now
+from shopping_cart import product_match
+# from shopping_cart import You_Pay
 
 
 
@@ -16,3 +20,16 @@ def human_friendly_time():
     time2 = now(6.43)
     assert time1 == "18:52"
     assert time2 == "6:43"
+
+def test_product_match():
+    products = [
+        {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
+        {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
+    ]
+
+    matching_product = product_match("2", products)
+    assert matching_product["name"] == "All-Seasons Salt"
+
+    with pytest.raises(IndexError):
+        product_match("2222", products)
+
